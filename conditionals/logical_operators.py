d89ba8abeeae8a0d7f852255a2b4b6e92ba46679
Séamus <ryans40@tcd.ie>
2022-11-08 11:50:37 +0000

    Adding conditionals letter files into their own folder

---
+++ conditionals/logical_operators.py
@@ -0,0 +1,10 @@
+ben_hungry = False
+ben_tired = False
+
+if ben_hungry and ben_tired:
+    print("Ben is hangry")
+
+if ben_hungry or ben_tired:
+    print("Ben needs food or sleep")
+else:
+    print("Ben's okay")
--- day_2_b.py
+++
@@ -1,8 +0,0 @@
-score = 6
-
-if score > 5:
-    result = "pass"
-else:
-    result = "fail"
-
-print(result)
