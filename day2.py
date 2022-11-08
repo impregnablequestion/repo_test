2e810296da2d679cf46ebcdbaff9151e7a107cc5
Séamus <ryans40@tcd.ie>
2022-11-08 11:21:44 +0000

    new file with result

--- day2.py
+++ day2.py
@@ -2,8 +2,19 @@
 # SET is_it_raining = INPUT "is it raining?"
 # IF is_it_raining is "yes"
 #   THEN PRINT "You should take the car"
+# ELSE 
+#   IF distance <5
+#       THEN PRINT "You should walk or cycle"
+#   ELSE
+#       THEN PRINT "You should get the bus"
+#END#
 
+distance = 6
 is_it_raining = input("Is it raining?")
 
 if is_it_raining == "yes":
     print("You should take the car")
+elif distance < 5:
+    print("You should walk or cycle")
+else:
+    print("Go on, take the bus")
---
+++ day_2_b.py
@@ -0,0 +1,8 @@
+score = 6
+
+if score > 5:
+    result = "pass"
+else:
+    result = "fail"
+
+print(result)
