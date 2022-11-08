718561cf53f0e4178303cf14f45de23acd699873
Séamus <ryans40@tcd.ie>
2022-11-08 16:31:02 +0000

    final commit of the day

--- loops/loops.py
+++ loops/loops.py
@@ -16,9 +16,32 @@
 
 # print("yeah well done!")
 
-while(True):
-    line = input("type something: ")
-    if(line.lower() == 'q'):
-        break
-    print(f"you typed {line}")
+# while(True):
+#     line = input("type something: ")
+#     if(line.lower() == 'q'):
+#         break
+#     print(f"you typed {line}")
 
+# numbers = [1, 2, 3, 4, 5]
+# for number in numbers:
+#     print(number * 3)
+
+# total = 0
+# for number in numbers:
+#     total = total + number
+# print(total)
+
+# chickens = ['Margaret', "Hetty", "Henrietta", "Audrey", "Mabel"]
+# for chicken in chickens:
+#     print(chicken)
+
+chickens = [
+  {"name": "Margaret", "age": 2, "eggs": 0},
+  {"name": "Hetty", "age": 1, "eggs": 2},
+  {"name": "Henrietta", "age": 3, "eggs": 1},
+  {"name": "Audrey", "age": 2, "eggs": 0},
+  {"name": "Mabel", "age": 5, "eggs": 1},
+]
+
+for chicken in chickens:
+    print(f'{chicken["name"]} is {chicken["age"]}')
